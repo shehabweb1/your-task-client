e259b39091644b4b202d83e49bd59c5650736b04
MD Shehab Hossen <shehab.web1@gmail.com>
2023-12-21 22:06:16 +0600

    Update 404 Page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,14 +1,21 @@
+import Helmet from "react-helmet";
 import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
 	return (
-		<div className="text-center mx-auto py-20">
-			<h1 className="text-7xl font-extrabold">404</h1>
-			<h3 className="text-3xl font-bold my-5"> This page is not found</h3>
-			<Link to="/">
-				<button className="btn btn-primary">Go to Home</button>
-			</Link>
-		</div>
+		<>
+			<Helmet>
+				<title>This page is not found</title>
+			</Helmet>
+
+			<div className="text-center mx-auto py-20">
+				<h1 className="text-7xl font-extrabold">404</h1>
+				<h3 className="text-3xl font-bold my-5">This page is not found</h3>
+				<Link to="/">
+					<button className="btn btn-primary">Go to Home</button>
+				</Link>
+			</div>
+		</>
 	);
 };
 
